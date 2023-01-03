51cfd39433a5a8aa9d27aa5215b6dc2a82a0b943
RUBETVENTURES <111089834+RUBETVENTURES@users.noreply.github.com>
2023-01-03 16:50:09 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,123 +1,142 @@
 #!/usr/bin/python3
 """Solves the N-queens puzzle.
+
 Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
+
 Example:
     $ ./101-nqueens.py N
+
 N must be an integer greater than or equal to 4.
+
 Attributes:
     board (list): A list of lists representing the chessboard.
     solutions (list): A list of lists containing solutions.
+
 Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
 where `r` and `c` represent the row and column, respectively, where a
 queen must be placed on the chessboard.
 """
 import sys
 
-def sys_chromosome(size): #making random chromosomes 
-    return [ sys.randint(1, nq) for _ in range(nq) ]
 
-def fitness(chromosome):
-    horizontal_collisions = sum([chromosome.count(queen)-1 for queen in chromosome])/2
-    diagonal_collisions = 0
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
 
-    n = len(chromosome)
-    left_diagonal = [0] * 2*n
-    right_diagonal = [0] * 2*n
-    for i in range(n):
-        left_diagonal[i + chromosome[i] - 1] += 1
-        right_diagonal[len(chromosome) - i + chromosome[i] - 2] += 1
 
-    diagonal_collisions = 0
-    for i in range(2*n-1):
-        counter = 0
-        if left_diagonal[i] > 1:
-            counter += left_diagonal[i]-1
-        if right_diagonal[i] > 1:
-            counter += right_diagonal[i]-1
-        diagonal_collisions += counter / (n-abs(i-n+1))
-    
-    return int(maxFitness - (horizontal_collisions + diagonal_collisions)) #28-(2+3)=23
+def board_deepcopy(board):
+    """Return a deepcopy of a chessboard."""
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return (board)
 
-def probability(chromosome, fitness):
-    return fitness(chromosome) / maxFitness
 
-def sys_pick(population, probabilities):
-    populationWithProbabilty = zip(population, probabilities)
-    total = sum(w for c, w in populationWithProbabilty)
-    r = sys.uniform(0, total)
-    upto = 0
-    for c, w in zip(population, probabilities):
-        if upto + w >= r:
-            return c
-        upto += w
-    assert False, "Shouldn't get here"
-        
-def reproduce(x, y): #doing cross_over between two chromosomes
-    n = len(x)
-    c = sys.randint(0, n - 1)
-    return x[0:c] + y[c:n]
+def get_solution(board):
+    """Return the list of lists representation of a solved chessboard."""
+    solution = []
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c] == "Q":
+                solution.append([r, c])
+                break
+    return (solution)
 
-def mutate(x):  #randomly changing the value of a random index of a chromosome
-    n = len(x)
-    c = sys.randint(0, n - 1)
-    m = sys.randint(1, n)
-    x[c] = m
-    return x
 
-def genetic_queen(population, fitness):
-    mutation_probability = 0.03
-    new_population = []
-    probabilities = [probability(n, fitness) for n in population]
-    for i in range(len(population)):
-        x = sys_pick(population, probabilities) #best chromosome 1
-        y = sys_pick(population, probabilities) #best chromosome 2
-        child = reproduce(x, y) #creating two new chromosomes from the best 2 chromosomes
-        if random.random() < mutation_probability:
-            child = mutate(child)
-        print_chromosome(child)
-        new_population.append(child)
-        if fitness(child) == maxFitness: break
-    return new_population
+def xout(board, row, col):
+    """X out spots on a chessboard.
 
-def print_chromosome(chrom):
-    print("Chromosome = {},  Fitness = {}"
-        .format(str(chrom), fitness(chrom)))
+    All spots where non-attacking queens can no
+    longer be played are X-ed out.
+
+    Args:
+        board (list): The current working chessboard.
+        row (int): The row where a queen was last played.
+        col (int): The column where a queen was last played.
+    """
+    # X out all forward spots
+    for c in range(col + 1, len(board)):
+        board[row][c] = "x"
+    # X out all backwards spots
+    for c in range(col - 1, -1, -1):
+        board[row][c] = "x"
+    # X out all spots below
+    for r in range(row + 1, len(board)):
+        board[r][col] = "x"
+    # X out all spots above
+    for r in range(row - 1, -1, -1):
+        board[r][col] = "x"
+    # X out all spots diagonally down to the right
+    c = col + 1
+    for r in range(row + 1, len(board)):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    # X out all spots diagonally up to the left
+    c = col - 1
+    for r in range(row - 1, -1, -1):
+        if c < 0:
+            break
+        board[r][c]
+        c -= 1
+    # X out all spots diagonally up to the right
+    c = col + 1
+    for r in range(row - 1, -1, -1):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    # X out all spots diagonally down to the left
+    c = col - 1
+    for r in range(row + 1, len(board)):
+        if c < 0:
+            break
+        board[r][c] = "x"
+        c -= 1
+
+
+def recursive_solve(board, row, queens, solutions):
+    """Recursively solve an N-queens puzzle.
+
+    Args:
+        board (list): The current working chessboard.
+        row (int): The current working row.
+        queens (int): The current number of placed queens.
+        solutions (list): A list of lists of solutions.
+    Returns:
+        solutions
+    """
+    if queens == len(board):
+        solutions.append(get_solution(board))
+        return (solutions)
+
+    for c in range(len(board)):
+        if board[row][c] == " ":
+            tmp_board = board_deepcopy(board)
+            tmp_board[row][c] = "Q"
+            xout(tmp_board, row, c)
+            solutions = recursive_solve(tmp_board, row + 1,
+                                        queens + 1, solutions)
+
+    return (solutions)
+
 
 if __name__ == "__main__":
-    nq = int(input("Enter Number of Queens: ")) #say N = 8
-    maxFitness = (nq*(nq-1))/2  # 8*7/2 = 28
-    population = [sys_chromosome(nq) for _ in range(100)]
-    
-    generation = 1
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-    while not maxFitness in [fitness(chrom) for chrom in population]:
-        print("=== Generation {} ===".format(generation))
-        population = genetic_queen(population, fitness)
-        print("")
-        print("Maximum Fitness = {}".format(max([fitness(n) for n in population])))
-        generation += 1
-    chrom_out = []
-    print("Solved in Generation {}!".format(generation-1))
-    for chrom in population:
-        if fitness(chrom) == maxFitness:
-            print("");
-            print("One of the solutions: ")
-            chrom_out = chrom
-            print_chromosome(chrom)
-            
-    board = []
-
-    for x in range(nq):
-        board.append(["x"] * nq)
-        
-    for i in range(nq):
-        board[nq-chrom_out[i]][i]="Q"
-            
-
-    def print_board(board):
-        for row in board:
-            print (" ".join(row))
-            
-    print()
-    print_board(board)
+    board = init_board(int(sys.argv[1]))
+    solutions = recursive_solve(board, 0, 0, [])
+    for sol in solutions:
+        print(sol)
